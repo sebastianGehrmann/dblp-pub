770debc55f3d47c871d6f71566adb0fe1a1cc8b4
Sebastian Gehrmann <s.gehrmann@outlook.com>
2015-09-25 15:39:35 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='dblp-pub',
+    name='dblp',
     version='0.1.0',
     author='Sebastian Gehrmann',
     author_email='gehrmann@seas.harvard.edu',
-    packages=['dblp-pub'],
+    packages=['dblp'],
     url='https://github.com/sebastianGehrmann/dblp-pub/tree/master',
     license='LICENSE.txt',
     description='Downloads and formats search results from dblp',
